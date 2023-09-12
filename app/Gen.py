75399f53715e13d44bfdb04ee9f3a5019225d6a4
ntgspecial <100434774+ntgspecial@users.noreply.github.com>
2023-09-12 16:51:21 +0530

    Create Gen.py

---
+++ app/Gen.py
@@ -0,0 +1,9 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv("API_HASH")
+
+async with TelegramClient(StringSession(), api_id, api_hash) as client:
+    session_string = client.session.save()
+    print(f"Your session string:\n{session_string}")
